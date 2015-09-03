c051127586e27c7896158c6aab3568faa546a6a5
Ekevoo <x@ekevoo.com>
2015-09-03 01:38:00 -0400

    Our Python3 mission is DONE!

--- pull.py
+++ pull.py
@@ -1,12 +1,13 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from json import dump, load
+from os.path import dirname
+from re import compile, DOTALL, IGNORECASE, MULTILINE
+from urllib.request import urlopen
 
-import feedparser
-import os.path
-import re
-import urllib2
+from feedparser import parse
 
-BASEPATH = os.path.dirname(__file__) + '/'
-# feedparser.USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.91 Safari/537.11'
+BASEPATH = dirname(__file__) + '/'
 
 
 class VideoFeed:
@@ -19,24 +20,24 @@ class VideoFeed:
 
     def fetch(self):
         try:
-            feed = feedparser.parse(self.url)
+            feed = parse(self.url)
             items = feed.entries
             if len(items) == 0:
                 raise Exception('Empty feed. Is site down?')
-            new_items = filter(self.is_new, items)
-            print 'Feed contains %d items, %d are new.' % (len(items), len(new_items))
+            new_items = list(filter(self.is_new, items))
+            print('Feed contains %d items, %d are new.' % (len(items), len(new_items)))
             # iterate first, then save new read list
             for i in new_items:
                 yield i
             self.read_list.save()
         except:
-            print 'Unexpected error with', self.url
-            print sys.exc_info()[0]
+            print('Unexpected error with', self.url)
+            print(sys.exc_info()[0])
 
 
 class EquestriaDailyVideoFeed(VideoFeed):
-    re_contents = re.compile("<div class='post-body .+<div class='post-footer'>", re.DOTALL | re.IGNORECASE)
-    re_youtube = re.compile('\\b(v[=/]|embed/|youtu\.be/)([A-Za-z0-9_-]{11})(?![A-Za-z0-9_-])', re.MULTILINE)
+    re_contents = compile("<div class='post-body .+<div class='post-footer'>", DOTALL | IGNORECASE)
+    re_youtube = compile('\\b(v[=/]|embed/|youtu\.be/)([A-Za-z0-9_-]{11})(?![A-Za-z0-9_-])', MULTILINE)
 
     def __init__(self, label=None):
         uri = 'http://www.equestriadaily.com/feeds/posts/default'
@@ -47,26 +48,26 @@ class EquestriaDailyVideoFeed(VideoFeed):
     def fetch_pages(self):
         for i in self.fetch():
             try:
-                print i.id, i.link
-                print 'Fetching ' + i.title
-                response = urllib2.urlopen(i.link)
-                html = response.read()
-                print 'Read %d kibibytes.' % (len(html) / 1024,)
+                print(i.id, i.link)
+                print('Fetching ' + i.title)
+                response = urlopen(i.link)
+                html = response.read().decode(errors='replace')
+                print('Read %d kibibytes.' % (len(html) / 1024,))
                 match = self.re_contents.search(html)
                 if match:
                     html = match.group(0)  # strip out header, footer, author, etc
                     yield html
                 else:
-                    print 'WARNING: No content.'
+                    print('WARNING: No content.')
             except:
-                print 'Unexpected error:', sys.exc_info()[0]
-            print
+                print('Unexpected error:', sys.exc_info())
+            print()
 
     def fetch_video_codes(self):
         for html in self.fetch_pages():
             for match in self.re_youtube.finditer(html):
                 video = match.group(2)
-                print 'Found video:', video
+                print('Found video:', video)
                 yield video
 
 
@@ -77,12 +78,12 @@ class YoutubeChannelVideoFeed(VideoFeed):
     def fetch_video_codes(self):
         for item in self.fetch():
             video = item.id[-11:]  # sometimes it can be this easy :)
-            print 'Found video: http://youtu.be/' + video
+            print('Found video: http://youtu.be/' + video)
             yield video
 
 
 class ReadList:  # this class is not thread-safe at all!
-    filename = BASEPATH + 'known.py'
+    filename = BASEPATH + 'known.json'
     lists = None
 
     def __init__(self, key):
@@ -90,30 +91,30 @@ class ReadList:  # this class is not thread-safe at all!
         if ReadList.lists is None:
             try:
                 f = open(ReadList.filename)
-                ReadList.lists = eval(f.read())
+                ReadList.lists = load(f)
                 f.close()
             except:
                 ReadList.lists = {}
         # instance initialization
         self.key = key
-        self.known = ReadList.lists.get(key, set())
-        self.new = set()
+        self.known = ReadList.lists.get(key, [])
+        self.new = []
 
     def is_new(self, item):
-        self.new.add(item)
+        self.new.append(item)
         return item not in self.known
 
     def save(self):
         ReadList.lists[self.key] = self.known = self.new
-        self.new = set()
+        self.new = []
         f = open(ReadList.filename, 'w')
-        f.write(repr(ReadList.lists))
+        dump(ReadList.lists, f, indent=4, sort_keys=True)
         f.close()
 
 
 def append_to_queue(video_feed):
     codes = video_feed.fetch_video_codes()
-    links = map(lambda v: '\nhttp://www.youtube.com/watch?v=' + v, codes)
+    links = ['\nhttp://www.youtube.com/watch?v=' + v for v in codes]
     f = open(BASEPATH + 'queue.txt', 'a')
     f.writelines(links)
     f.close()
@@ -144,7 +145,7 @@ if __name__ == '__main__':
     # append_to_queue(YoutubeChannelVideoFeed('UCs4br3aZLU0sOEM-3n0-6xQ'))  # yc Duncan
     # append_to_queue(YoutubeChannelVideoFeed('UCpu8dLHavjMi1a5jgT9ycMA'))  # yc Sjin
     # append_to_queue(YoutubeChannelVideoFeed('UCUxoapwoGN9cKN5SPKGVh7A'))  # yc Kim
-    # append_to_queue(YoutubeChannelVideoFeed('BdoubleO100')) mais mine via roo <3
     append_to_queue(YoutubeChannelVideoFeed('UCaYuAirih8a1Etht7KMuDaA'))  # heythatscool
     append_to_queue(YoutubeChannelVideoFeed('UCwf7GkXUML0Whgdt6lvMFTw'))  # Crowne Prince
     append_to_queue(YoutubeChannelVideoFeed('UCuUV_ANYYmZvdcUxbe5nH1Q'))  # OkashiNaArtist Nat
+    # append_to_queue(YoutubeChannelVideoFeed('UCDIotuC7vNpQJKsZGT47LPQ'))  # BronyCAN
