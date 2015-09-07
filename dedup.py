6884fc83415a70fbdabfd17fba53aaf61d63ddcb
Ekevoo <x@ekevoo.com>
2015-09-06 23:22:51 -0400

    Dedup is usable. Very nice.

--- dedup.py
+++ dedup.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from collections import defaultdict
-from logging import getLogger
+from logging import getLogger, StreamHandler, DEBUG
 from os import listdir, replace
 from os.path import splitext, join, dirname
 from re import compile
+from sys import argv
 
 from youtube_dl import list_extractors
 
-log = getLogger('dedup')
+log = getLogger('tubeforme.dedup')
 
 
 class Deduplicator:
-    def __init__(self, base, queue, inv):
+    def __init__(self, base, inv, queue):
         self._queuepath = join(base, queue)
         self._invpath = join(base, inv)
 
@@ -20,10 +21,12 @@ class Deduplicator:
         'youtube': compile(r'-([a-zA-Z0-9_-]{11})$'),
     }
 
-    @staticmethod
-    def url_service_and_code(url):
-        for ie in list_extractors(None):
-            if ie.working() and ie.suitable(url):
+    extractors = list(reversed(list_extractors(None)))  # Reversed to keep YouTube close to the top.
+
+    @classmethod
+    def url_service_and_code(cls, url):
+        for ie in cls.extractors:
+            if ie.working() and ie.suitable(url) and hasattr(ie, 'extract_id'):
                 return ie.IE_NAME, ie.extract_id(url)
         return None, None
 
@@ -45,19 +48,25 @@ class Deduplicator:
                     result[service].add(code)
         return result
 
-    def main(self):
+    def main(self, pretend=False):
         inventory = self.get_obtained_files()
         keep_those = []
         discarded_count = 0
-        log('Currently with %d files.', sum([len(s) for s in inventory.values()]))
+        log.debug('Currently with %d files.', sum([len(s) for s in inventory.values()]))
         for url in open(self._queuepath, 'r'):
+            if not url.strip():
+                continue  # Blank line, skip!
             service, code = self.url_service_and_code(url.strip())
             if service and code and code in inventory[service]:
                 discarded_count += 1
             else:
                 keep_those.append(url)
                 inventory[service].add(code)  # Discard future duplicates.
-        if discarded_count > 0:
+        if pretend:
+            log.debug('Discarded %d items. These stayed:', discarded_count)
+            for i in keep_those:
+                log.debug(i.strip())
+        elif discarded_count > 0:
             backup = self._queuepath + '~'
             replace(self._queuepath, backup)
             with open(self._queuepath, 'w') as f:
@@ -65,5 +74,7 @@ class Deduplicator:
 
 
 if __name__ == '__main__':
+    log.addHandler(StreamHandler())
+    log.level = DEBUG
     ded = Deduplicator(dirname(__file__), 'videos', 'queue.txt')
-    ded.main()
+    ded.main('save' not in argv)
