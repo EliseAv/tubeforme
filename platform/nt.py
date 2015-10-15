e7686649ab1327b7cb8d9c85edb699fc156ae598
Ekevoo <x@ekevoo.com>
2015-10-15 00:00:24 -0400

    Removed the last of Python 2 cruft. Yuck.

--- platform/nt.py
+++ platform/nt.py
@@ -11,7 +11,6 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 #
-from __future__ import absolute_import, unicode_literals
 from ctypes import c_ulonglong, pointer, c_wchar_p, windll
 
 
